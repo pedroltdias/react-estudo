fcd86f6a8e19641896e1b609075e715ce24467d4
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-19 17:00:51 -0300

    componente item

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "./Item";
 import style from './Lista.module.scss';
 
 function Lista() {
@@ -16,16 +17,16 @@ function Lista() {
             tempo: "03:00:00"
         }
     ];
-    return(
+    return (
         <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                        <li key={index} className={style.item}>
-                            <h3>{item.tarefa}</h3>
-                            <span>{item.tempo}</span>
-                        </li>
-                    )
+                    <Item 
+                        key={index}
+                        {...item}
+                    />
+                )
                 )}
             </ul>
         </aside>

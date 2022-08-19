39172f6a25c0576e6ae0c9e13bc39c4a5d064ba0
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-19 14:45:11 -0300

    adaptacao do codigo utilizando css modules

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
-import './style.scss';
+import style from './App.module.scss';
 
 function App() {
   return (
-    <div className="AppStyle">
+    <div className={style.AppStyle}>
       <Formulario />
-      <Lista/>
+      <Lista />
     </div>
   );
 }

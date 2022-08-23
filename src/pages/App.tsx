e15c86197ecddc06b2fbcf3e0af9ffa7d803b972
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-23 16:30:43 -0300

    funcionalidade para selecionar estudo

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,14 +6,26 @@ import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
 
 function App() {
-  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
-  return (
-    <div className={style.AppStyle}>
-      <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
-      <Cronometro />
-    </div>
-  );
+    const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+    const [selecionado, setSelecionado] = useState<ITarefa>();
+
+    function selecionaTarefa(tarefaSelecionada: ITarefa) {
+        setSelecionado(tarefaSelecionada);
+        setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+            ...tarefa,
+            selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+        })));
+    }
+    return (
+        <div className={style.AppStyle}>
+            <Formulario setTarefas={setTarefas} />
+            <Lista
+                tarefas={tarefas}
+                selecionaTarefa={selecionaTarefa}
+            />
+            <Cronometro />
+        </div>
+    );
 }
 
 export default App;

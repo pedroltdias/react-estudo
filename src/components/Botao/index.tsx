32d64556152d635a363a7fb08330ef915f0da90f
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-24 14:12:17 -0300

    funcao recursiva 'regressiva' para o funcionamento do cronometro

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{ children?: React.ReactNode, type?: "button" | "submit" | "reset" | undefined }> {
+class Botao extends React.Component<{
+    children?: React.ReactNode,
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void
+}> {
     render() {
-        const { type = "button" } = this.props;
+        const { type = "button", onClick } = this.props;
         return (
-            <button type={type} className={style.botao}>
+            <button onClick={onClick} type={type} className={style.botao}>
                 {this.props.children}
             </button>
         );

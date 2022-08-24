fdbf368407cb3f0567f4d6e13c60f6ed59c94a54
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-24 14:01:30 -0300

    exibicao dos tempo de cada estudo no cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,7 +2,7 @@ import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
 import { ITarefa } from "../../types/tarefa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../common/utils/time";
 
 interface Props {
@@ -10,16 +10,18 @@ interface Props {
 }
 
 export default function Cronometro({ selecionado }: Props) {
-    const [tempo, setTempo] = useState<Number>();
-    if (selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado?.tempo));
-    }
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado]);
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-            Tempo: {tempo}
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo} />
             </div>
             <Botao>
                 Começar!
